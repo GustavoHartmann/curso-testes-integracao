b5268aa5b48852441062d4cc777b618df9771ef9
Gustavo <gustavolerner.hartmann@gmail.com>
2023-09-26 16:20:47 -0300

    exercicio 3

---
+++ aula-testes-03/tests/calculator.test.ts
@@ -0,0 +1,17 @@
+describe("test calculator functions", () => {
+  it("test sum", () => {
+    expect(1 + 2).toBe(3);
+  });
+
+  it("test sub", () => {
+    expect(1 - 2).toBe(-1);
+  });
+
+  it("test mul", () => {
+    expect(1 * 2).toBe(2);
+  });
+
+  it("test div", () => {
+    expect(1 / 2).toBe(0.5);
+  });
+});
