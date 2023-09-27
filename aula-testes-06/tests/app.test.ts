56001eca21fbc6230e213e378e8a5c6bfd20ae13
Gustavo <gustavolerner.hartmann@gmail.com>
2023-09-27 13:58:13 -0300

    exercicio 6

---
+++ aula-testes-06/tests/app.test.ts
@@ -0,0 +1,21 @@
+import supertest from "supertest";
+
+import app from "./../src/app";
+
+const api = supertest(app);
+
+describe("API test", () => {
+  it("should return 200 /event", async () => {
+    const { statusCode, body } = await api.get("/event");
+
+    expect(statusCode).toBe(200);
+    expect(body).toEqual(
+      expect.objectContaining({
+        id: expect.any(Number),
+        title: expect.any(String),
+        image: expect.any(String),
+        date: expect.any(String),
+      })
+    );
+  });
+});
